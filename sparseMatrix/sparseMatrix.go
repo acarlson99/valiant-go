4427e9e6cabacdee5ee633dacbe4fdc6ff971f3d
Alexander <bombblob@gmail.com>
2023-12-12 00:24:35 -0800

    genericized

--- sparseMatrix/sparseMatrix.go
+++ sparseMatrix/sparseMatrix.go
@@ -1,49 +1,46 @@
 package sparseMatrix
 
-type Thing interface{}
-
-type Matrix interface {
-	Idx(i, j int) *Thing
-	Insert(i, j int, v *Thing) Matrix
+type Matrix[T any] interface {
+	Index(i, j int) T
+	Insert(i, j int, v T) Matrix[T]
 	Width() int
 	Height() int
 }
 
 // partition = N/2 where N is the width in cells of this section of the matrix
 
-type SquareMatrix struct {
-	UL *Matrix
-	UR *Matrix
-	BL *Matrix
-	BR *Matrix
+type SquareMatrix[T any] struct {
+	UL *Matrix[T]
+	UR *Matrix[T]
+	BL *Matrix[T]
+	BR *Matrix[T]
 
-	w int
+	w int // TODO: rebrand to `size` or actually track width+height, not this shit wtf
 }
 
-func NewSquareMatrix(n int) *SquareMatrix {
-	// i = int(math.Pow(math.Ceil(math.Sqrt(float64(i))), 2))
-	i := 1 << 0
+func NewSquareMatrix[T any](n int) *SquareMatrix[T] {
+	i := 1 << 0 // TODO: remove hack
 	for i < n {
 		i = i << 1
 	}
-	return &SquareMatrix{w: i}
+	return &SquareMatrix[T]{w: i}
 }
 
-func (m *SquareMatrix) Width() int {
+func (m *SquareMatrix[T]) Width() int {
 	if m == nil {
 		return 0
 	}
 	return m.w
 }
 
-func (m *SquareMatrix) Height() int {
+func (m *SquareMatrix[T]) Height() int {
 	if m == nil {
 		return 0
 	}
 	return m.w
 }
 
-func (m *SquareMatrix) quarter(i, j int) *Matrix {
+func (m *SquareMatrix[T]) quarter(i, j int) *Matrix[T] {
 	subI := m.w / 2
 	subJ := m.w / 2
 	var left bool
@@ -65,7 +62,7 @@ func (m *SquareMatrix) quarter(i, j int) *Matrix {
 	}
 }
 
-func (m *SquareMatrix) setQuarter(i, j int, mx *Matrix) *Matrix {
+func (m *SquareMatrix[T]) setQuarter(i, j int, mx *Matrix[T]) *Matrix[T] {
 	subI := m.w / 2
 	subJ := m.w / 2
 	var left bool
@@ -91,8 +88,7 @@ func (m *SquareMatrix) setQuarter(i, j int, mx *Matrix) *Matrix {
 	}
 }
 
-// n=2 w=2 => 0
-func safeHalve(n int, m *SquareMatrix) int {
+func partitionIndex[T any](n int, m *SquareMatrix[T]) int {
 	half := m.w / 2
 	if n < half {
 		return n
@@ -121,59 +117,66 @@ func safeHalve(n int, m *SquareMatrix) int {
 // X X
 // UR X,Y = 0,0
 
-func (m *SquareMatrix) Idx(i, j int) *Thing {
+func (m *SquareMatrix[T]) inBounds(i, j int) bool {
+	return i >= 0 && j >= 0 && i < m.w && j < m.w
+}
+
+func (m *SquareMatrix[T]) Index(i, j int) T {
 	if m == nil {
-		return nil
+		return *new(T)
 	}
 	q := m.quarter(i, j)
 	if q == nil {
-		return nil
+		return *new(T)
 	}
-	return (*q).Idx(safeHalve(i, m), safeHalve(j, m)) // TODO: this divide by 2 does not work
+	return (*q).Index(partitionIndex(i, m), partitionIndex(j, m))
 }
 
-func (m *SquareMatrix) Insert(i, j int, v *Thing) Matrix {
+func (m *SquareMatrix[T]) Insert(i, j int, v T) Matrix[T] {
+	if !m.inBounds(i, j) {
+		return nil
+	}
 	q := m.quarter(i, j)
 	if q == nil {
 		// TODO: decide what matrix to insert, should be square or unary
-		var mx Matrix
+		var mx Matrix[T]
 		if m.w == 2 {
-			mx = &UnitMatrix{}
+			mx = &UnitMatrix[T]{}
 		} else {
-			mx = &SquareMatrix{w: m.w / 2}
+			mx = &SquareMatrix[T]{w: m.w / 2}
 		}
-		mx = mx.Insert(safeHalve(i, m), safeHalve(j, m), v)
+		mx = mx.Insert(partitionIndex(i, m), partitionIndex(j, m), v)
 		m.setQuarter(i, j, &mx)
-	} else {
-		(*q).Insert(safeHalve(i, m), safeHalve(j, m), v)
+		return m
 	}
+	(*q).Insert(partitionIndex(i, m), partitionIndex(j, m), v)
 	return m
 }
 
-type UnitMatrix struct {
-	v *Thing
+type UnitMatrix[T any] struct {
+	v T
 }
 
-func (m *UnitMatrix) Width() int { return 1 }
+func (m *UnitMatrix[T]) Width() int { return 1 }
 
-func (m *UnitMatrix) Height() int { return 1 }
+func (m *UnitMatrix[T]) Height() int { return 1 }
 
-func (m *UnitMatrix) Idx(i, j int) *Thing {
+func (m *UnitMatrix[T]) Index(i, j int) T {
 	return m.v
 }
 
-func (m *UnitMatrix) Insert(i, j int, v *Thing) Matrix {
+func (m *UnitMatrix[T]) Insert(i, j int, v T) Matrix[T] {
 	if m == nil {
-		return &UnitMatrix{v: v}
+		return &UnitMatrix[T]{v: v}
 	}
 	m.v = v
 	return m
 }
 
-type UpperRightTriangularMatrix struct {
-	UR *SquareMatrix
-	UL *UpperRightTriangularMatrix
-	BR *UpperRightTriangularMatrix
+type UpperRightTriangularMatrix[T any] struct {
+	UR *SquareMatrix[T]
+	UL *UpperRightTriangularMatrix[T]
+	BR *UpperRightTriangularMatrix[T]
 
 	w, h int
 }
--- sparseMatrix/sparseMatrix_test.go
+++ sparseMatrix/sparseMatrix_test.go
@@ -1,85 +1,92 @@
 package sparseMatrix
 
 import (
+	"fmt"
 	"reflect"
-	"strconv"
 	"testing"
 )
-
-func TestNewSquareMatrix(t *testing.T) {
-	type args struct {
-		i int
-	}
-	tests := []struct {
-		name string
-		args args
-		want *SquareMatrix
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewSquareMatrix(tt.args.i); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewSquareMatrix() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 type Pair struct {
 	x int
 	y int
 }
 
-func TestSquareMatrix(t *testing.T) {
-	// TODO: size of 2 breaks, why lmao
-	for size := 3; size < 2000; size++ {
-		t.Run(strconv.Itoa(size), func(t *testing.T) {
-			sq := NewSquareMatrix(size)
-			for i := 0; i < size; i++ {
-				for j := 0; j < size; j++ {
-					var want *Thing
-					if got := sq.Idx(i, j); !reflect.DeepEqual(got, want) {
-						t.Errorf("i,j=%d,%d expected nil: got %v want %v", i, j, got, want)
-					}
-					var p Thing
-					p = Pair{x: i, y: j}
-					sq.Insert(i, j, &p)
-					if got, want := sq.Idx(i, j), &p; !reflect.DeepEqual(got, want) {
-						t.Errorf("i,j=%d,%d just set: got %v want %v", i, j, got, want)
-					}
-				}
-			}
-			for i := 0; i < size; i++ {
-				for j := 0; j < size; j++ {
-					var p Thing
-					p = Pair{x: i, y: j}
-					if got, want := sq.Idx(i, j), &p; !reflect.DeepEqual(got, want) {
-						t.Errorf("i,j=%d,%d read old: got %v want %v", i, j, got, want)
-					}
-				}
-			}
+// TODO: invalid writes should not write (-1,-1 for instance)
+func populateCanary(m Matrix[string]) {
+	for i := 0; i < m.Width(); i++ {
+		for j := 0; j < m.Width(); j++ {
+			var str string
+			str = fmt.Sprintf("CANARY %d,%d", i, j)
+			m.Insert(i, j, str)
+		}
+	}
+}
 
-			for _, tc := range []Pair{
-				{x: 2, y: 2},
-			} {
-				var p Thing
-				p = tc
-				sq.Insert(tc.x, tc.y, &p)
+func TestSquareMatrix(t *testing.T) {
+	for size := 2; size < 200; size++ {
+		t.Run(fmt.Sprintf("iter-size-%d", size), func(t *testing.T) {
+			t.Run("no-overwrite", func(t *testing.T) {
+				sq := NewSquareMatrix[*Pair](size)
 				for i := 0; i < size; i++ {
 					for j := 0; j < size; j++ {
-						if i == tc.x && j == tc.y {
-							continue
+						if got, want := sq.Index(i, j), (*Pair)(nil); !reflect.DeepEqual(got, want) {
+							t.Errorf("i,j=%d,%d expected nil: got %v want %v", i, j, got, want)
 						}
-						var str Thing
-						str = "FUCKNOOO"
-						sq.Insert(i, j, &str)
+						p := &Pair{x: i, y: j}
+						sq.Insert(i, j, p)
+						if got, want := sq.Index(i, j), p; !reflect.DeepEqual(got, want) {
+							t.Errorf("i,j=%d,%d just set: got %v want %v", i, j, got, want)
+						}
 					}
 				}
-
-				if got, want := sq.Idx(tc.x, tc.y), p; *got != want {
-					t.Errorf("i,j=%d,%d read old: got %v want %v", tc.x, tc.y, got, want)
+				for i := 0; i < size; i++ {
+					for j := 0; j < size; j++ {
+						p := &Pair{x: i, y: j}
+						if got, want := sq.Index(i, j), p; !reflect.DeepEqual(got, want) {
+							t.Errorf("i,j=%d,%d read old: got %v want %v", i, j, got, want)
+						}
+					}
 				}
+			})
+
+			t.Run("edges", func(t *testing.T) {
+				sq := NewSquareMatrix[Pair](size)
+				// populateCanary(sq)
+				testCases := []Pair{
+					{x: 0, y: 0},
+					{x: size - 1, y: size - 1},
+					{x: 0, y: size - 1},
+					{x: size - 1, y: 0},
+					{x: size / 2, y: size / 3},
+					{x: size / 3, y: size / 2},
+				}
+				for _, tc := range testCases {
+					sq.Insert(tc.x, tc.y, tc)
+				}
+				for _, tc := range testCases {
+					if got, want := sq.Index(tc.x, tc.y), tc; got != want {
+						t.Errorf("i,j=%d,%d read old: got %v want %v", tc.x, tc.y, got, want)
+					}
+				}
+			})
+
+			// only on N^2 bc of how size is calculated
+			if size&(size-1) == 0 {
+				t.Run("out of bounds", func(t *testing.T) {
+					sq := NewSquareMatrix[string](size)
+					for _, tc := range []struct {
+						x int
+						y int
+						s string
+					}{{-1, -1, "hello :)"},
+						{size, size, "wowww"},
+						{size, 0, "hehe"}} {
+						sq.Insert(tc.x, tc.y, tc.s)
+						if sq.UL != nil || sq.UR != nil || sq.BL != nil || sq.BR != nil {
+							t.Fatalf("Invalid inputs %v should have written to no cells", tc)
+						}
+					}
+				})
 			}
 		})
 	}
