8e5769e496e186e2c1fe8b326051623bb7232bc4
a <a@a.a>
2023-12-22 16:48:04 -0800

    todo: lotta stuff

--- valiant-hs/src/SparseMatrix.hs
+++ valiant-hs/src/SparseMatrix.hs
@@ -6,13 +6,13 @@ data Matrix m
   = SquareMatrix Int (Matrix m) (Matrix m) (Matrix m) (Matrix m)
   | UpperRightTriangularMatrix Int (Matrix m) (Matrix m) (Matrix m)
   | UnitMatrix m
-  | Empty
+  | Empty Int -- (Empty 2) is same size as (SquareMatrix 2 _ _ _ _); placeholder value
   deriving (Eq)
 
 newUpperRightTriangularMatrix :: Int -> Matrix m
 newUpperRightTriangularMatrix n
-  | nsq < 2 = Empty
-  | nsq == 2 = UpperRightTriangularMatrix nsq Empty Empty Empty
+  | nsq < 2 = Empty 0
+  | nsq == 2 = UpperRightTriangularMatrix nsq (Empty nsq) (Empty nsq) (Empty nsq)
   | otherwise = UpperRightTriangularMatrix nsq smallT smallSQ smallT
   where
     smallT = newUpperRightTriangularMatrix (float2Int (int2Float n / 2.0))
@@ -25,8 +25,8 @@ nextClosestSquare n =
 
 newSquareMatrix :: Int -> Matrix m
 newSquareMatrix n
-  | nsq < 2 = Empty
-  | nsq == 2 = SquareMatrix nsq Empty Empty Empty Empty
+  | nsq < 2 = Empty 0
+  | nsq == 2 = SquareMatrix nsq (Empty nsq) (Empty nsq) (Empty nsq) (Empty nsq)
   | otherwise = SquareMatrix nsq smallM smallM smallM smallM
   where
     smallM = newSquareMatrix (float2Int (int2Float n / 2.0))
@@ -35,28 +35,15 @@ newSquareMatrix n
 -- v :: SquareMatrix a -> SquareMatrix a -> SquareMatrix a -> SquareMatrix a
 -- v a y b = undefined
 
--- printMatrix :: Matrix mx -> (String, Int)
--- printMatrix a n = undefined
-
-data Tree = Leaf Int | Node Tree Tree
-
-repmin :: Tree -> Tree
-repmin t = tr where (mn, tr) = walk mn t
-
-walk :: Int -> Tree -> (Int, Tree)
-walk mn (Leaf n) = (n, Leaf mn)
-walk mn (Node t1 t2) = (n1 `min` n2, Node tr1 tr2)
-  where
-    (n1, tr1) = walk mn t1
-    (n2, tr2) = walk mn t2
-
 fixLength :: Int -> [Char] -> [Char]
-fixLength n s = replicate (n - length s) ' ' ++ s ++ " "
+fixLength n s = replicate md ' ' ++ s ++ replicate dv ' '
+  where
+    (dv, md) = n `divMod` length s
 
 concatQuads :: String -> String -> String -> String -> String
 concatQuads a b c d = upper ++ lower
   where
-    lhf = map (++ " ") . lines
+    lhf = lines
     rhf = map (++ "\n") . lines
     a_ = lhf a
     b_ = rhf b
@@ -66,31 +53,45 @@ concatQuads a b c d = upper ++ lower
     upper = concat $ zipWith (++) a_ b_
     lower = concat $ zipWith (++) c_ d_
 
-applyF :: [t -> c] -> [t] -> [c]
-applyF = zipWith (\a b -> a b)
+applyF :: (String -> String -> a) -> [String] -> [String] -> [a]
+applyF f (a : as) (b : bs) = f a b : applyF f as bs
+applyF f [] (b : bs) = f (replicate (length b) ' ') b : applyF f [] bs
+applyF f (a : as) [] = f a (replicate (length a) ' ') : applyF f as []
+applyF _ [] [] = []
 
 showMx :: Show m => Matrix m -> String
 showMx m = s where (mx, s) = walkMx mx m
 
 walkMx :: Show m => Int -> Matrix m -> (Int, String)
-walkMx mx Empty = (0, fixLength mx "")
+walkMx mx (Empty _) = (0, fixLength mx "")
 walkMx mx (UnitMatrix m) = (length s, fixLength mx s)
   where
     s = show m
-walkMx mx (SquareMatrix _ a b c d) = (foldr max 0 ns, concatQuads (head strs) (strs !! 1) (strs !! 2) (strs !! 3))
+walkMx mx (SquareMatrix _ a b c d) = (foldr max 0 ns, concatQuads sa sb sc sd)
   where
-    (ns, strs) = unzip $ map (mxMaybe (0, "") (walkMx mx)) [a, b, c, d]
-walkMx _ _ = undefined
+    (ns, [sa, sb, sc, sd]) = unzip $ map (walkMx mx) [a, b, c, d]
+walkMx mx (UpperRightTriangularMatrix size a b d) = (foldr max 0 ns, concatQuads sa sb (replicate size '\n') sd)
+  where
+    (ns, [sa, sb, sd]) = unzip $ map (walkMx mx) [a, b, d]
+
+-- (ns, [sa, sb, sc, sd]) = unzip $ map (walkMx mx) [a, b, c, SquareMatrix 0 (Empty (float2Int (int2Float size / 2.0))) (Empty (float2Int (int2Float size / 2.0))) (Empty (float2Int (int2Float size / 2.0))) (Empty (float2Int (int2Float size / 2.0)))]
+
+-- walkMx mx (UpperRightTriangularMatrix size a b c) = undefined
+-- walkMx mx (UpperRightTriangularMatrix _ a b c) = concatQuads ()
+--   where
+--     (ns, strs) = unzip $ map (mxMaybe (0, "") (walkMx mx)) [a, b, c]
 
 mxMaybe :: p -> (Matrix m -> p) -> Matrix m -> p
-mxMaybe b _ Empty = b
+mxMaybe b _ (Empty _) = b
 mxMaybe _ f m = f m
 
+-- mat = SquareMatrix 4 a a a a
 mat :: Matrix Int
-mat = SquareMatrix 4 a a a a
+mat = UpperRightTriangularMatrix 4 t a t
   where
     a = SquareMatrix 2 b c d e
-    b = UnitMatrix 4 :: Matrix Int
+    b = UnitMatrix 400 :: Matrix Int
     c = UnitMatrix 6 :: Matrix Int
     d = UnitMatrix 9 :: Matrix Int
-    e = UnitMatrix 200 :: Matrix Int
+    e = UnitMatrix 2 :: Matrix Int
+    t = UpperRightTriangularMatrix 2 (UnitMatrix 1) (UnitMatrix 2) (UnitMatrix 3)
