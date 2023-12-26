4e81e3d2f8a84714b828de7762c5610092419569
a <a@a.a>
2023-12-26 00:18:36 -0800

    Applicative Functor

--- valiant-hs/src/SparseMatrix.hs
+++ valiant-hs/src/SparseMatrix.hs
@@ -12,10 +12,24 @@ data Matrix m
   deriving (Eq)
 
 instance Functor Matrix where
-  fmap f (SquareMatrix s a b c d) = SquareMatrix s (f a) (f b) (f c) (f d)
-  fmap f (UpperRightTriangularMatrix s a b d) = UpperRightTriangularMatrix s (f a) (f b) (f d)
+  fmap :: (a -> b) -> Matrix a -> Matrix b
+  fmap f (SquareMatrix n a b c d) = SquareMatrix n (fmap f a) (fmap f b) (fmap f c) (fmap f d)
+  fmap f (UpperRightTriangularMatrix n a b d) = UpperRightTriangularMatrix n (fmap f a) (fmap f b) (fmap f d)
   fmap f (UnitMatrix a) = UnitMatrix (f a)
   fmap _ (Empty size) = Empty size
+
+instance Applicative Matrix where
+  pure :: a -> Matrix a
+  pure = UnitMatrix
+  (<*>) :: Matrix (a -> b) -> Matrix a -> Matrix b
+  (SquareMatrix n1 f1 f2 f3 f4) <*> (SquareMatrix n2 a b c d)
+    | n1 /= n2 = error "Matrix size mismatch!"
+    | otherwise = SquareMatrix n1 (f1 <*> a) (f2 <*> b) (f3 <*> c) (f4 <*> d)
+  (UpperRightTriangularMatrix n1 f1 f2 f3) <*> (UpperRightTriangularMatrix n2 a b c)
+    | n1 /= n2 = error "Matrix size mismatch!"
+    | otherwise = UpperRightTriangularMatrix n1 (f1 <*> a) (f2 <*> b) (f3 <*> c)
+  (UnitMatrix f) <*> (UnitMatrix x) = UnitMatrix (f x)
+  _ <*> _ = error "Matrix type mismatch!"
 
 newUpperRightTriangularMatrix :: Show m => Int -> Matrix m
 newUpperRightTriangularMatrix n
@@ -45,11 +59,12 @@ newSquareMatrix n
 -- v a y b = undefined
 
 instance Show m => Show (Matrix m) where
+  show :: Show m => Matrix m -> String
   show mat = str
     where
       (topMax, str) = walk mat
       -- Richard Bird repmin-like function for printing a matrix
-      walk :: Show m => Matrix m -> (Int, String)
+      walk :: (Show m) => Matrix m -> (Int, String)
       walk (Empty size) = (0, concat . replicate size $ replicate ((topMax + 1) * size) ' ' ++ "\n")
       walk (UnitMatrix m) = (length s, fixLength topMax s)
         where
@@ -66,6 +81,7 @@ instance Show m => Show (Matrix m) where
       walk (UpperRightTriangularMatrix size a b d) = (foldr max 0 ns, concatQuads sa sb sc sd)
         where
           (ns, [sa, sb, sc, sd]) = unzip $ map walk [a, b, Empty (float2Int (int2Float size / 2.0)), d]
+
       concatQuads :: String -> String -> String -> String -> String
       concatQuads a b c d = concatMap pairConcat [(a, b), (c, d)]
         where
