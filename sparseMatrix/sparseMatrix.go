fb66710efc433605de5fe0b3f8e103279481b44c
Alexander <bombblob@gmail.com>
2024-01-22 20:01:55 -0800

    print, tests

--- sparseMatrix/sparseMatrix.go
+++ sparseMatrix/sparseMatrix.go
@@ -3,6 +3,7 @@ package sparseMatrix
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 type Matrix[T any] interface {
@@ -12,12 +13,11 @@ type Matrix[T any] interface {
 	Insert(i, j int, v T) Matrix[T]
 	Width() int
 	Height() int
+
+	String() string
 }
 
 type SquareMatrix[T any] struct {
-	Ring[T]
-	Matrix[T]
-
 	UL Matrix[T]
 	UR Matrix[T]
 	BL Matrix[T]
@@ -112,13 +112,53 @@ func (m *SquareMatrix[T]) Multiply(b Ring[T]) Ring[T] {
 	return nil
 }
 
-func NewSquareMatrix[T any](n int) *SquareMatrix[T] {
-	// func NewSquareMatrix[T any](n int) *SquareMatrix[T] {
+func nextSquare(n int) int {
 	i := 1 << 0 // TODO: remove hack
 	for i < n {
 		i = i << 1
 	}
-	return &SquareMatrix[T]{w: i}
+	return i
+}
+
+func (m *SquareMatrix[T]) String() string {
+	if m == nil {
+		return ""
+	}
+	UL := m.UL
+	if UL == nil {
+		UL = &EmptyMatrix[T]{w: m.w >> 1}
+	}
+	a := strings.Split((UL).String(), "\n")
+	UR := m.UR
+	if UR == nil {
+		UR = &EmptyMatrix[T]{w: m.w >> 1}
+	}
+	b := strings.Split((UR).String(), "\n")
+	BL := m.BL
+	if BL == nil {
+		BL = &EmptyMatrix[T]{w: m.w >> 1}
+	}
+	c := strings.Split((BL).String(), "\n")
+	BR := m.BR
+	if BR == nil {
+		BR = &EmptyMatrix[T]{w: m.w >> 1}
+	}
+	d := strings.Split((BR).String(), "\n")
+
+	for i, v := range b {
+		a[i] = strings.Join([]string{a[i], v}, "")
+	}
+	for i, v := range d {
+		c[i] = strings.Join([]string{c[i], v}, "")
+	}
+
+	return strings.Join([]string{strings.Join(a, "\n"), strings.Join(c, "\n")}, "\n")
+}
+
+func NewSquareMatrix[T any](n int) *SquareMatrix[T] {
+	i := nextSquare(n)
+	return &SquareMatrix[T]{UL: &EmptyMatrix[T]{w: i >> 1}, UR: &EmptyMatrix[T]{w: i >> 1}, BL: &EmptyMatrix[T]{w: i >> 1}, BR: &EmptyMatrix[T]{w: i >> 1}, w: i}
+	// return &SquareMatrix[T]{w: i}
 }
 
 func (m *SquareMatrix[T]) Width() int {
@@ -234,24 +274,28 @@ func (m *SquareMatrix[T]) Index(i, j int) T {
 	return q.Index(partitionIndex(i, m), partitionIndex(j, m))
 }
 
+func IsEmpty[T any](m Matrix[T]) bool {
+	switch m.(type) {
+	case *EmptyMatrix[T]:
+		return true
+	default:
+		return false
+	}
+}
+
 func (m *SquareMatrix[T]) Insert(i, j int, v T) Matrix[T] {
 	if !m.inBounds(i, j) {
 		return nil
 	}
 	q := m.quarter(i, j)
-	if q == nil {
-		// TODO: decide what matrix to insert, should be square or unary
-		var mx Matrix[T]
-		if m.w == 2 {
-			mx = &UnitMatrix[T]{}
+	if q == nil || IsEmpty[T](q) {
+		if m.w <= 2 {
+			q = &UnitMatrix[T]{}
 		} else {
-			mx = &SquareMatrix[T]{w: m.w / 2}
+			q = NewSquareMatrix[T](m.w >> 1)
 		}
-		mx = mx.Insert(partitionIndex(i, m), partitionIndex(j, m), v)
-		m.setQuarter(i, j, mx)
-		return m
 	}
-	q.Insert(partitionIndex(i, m), partitionIndex(j, m), v)
+	m.setQuarter(i, j, q.Insert(partitionIndex(i, m), partitionIndex(j, m), v))
 	return m
 }
 
@@ -283,10 +327,119 @@ func (m *UnitMatrix[T]) Insert(i, j int, v T) Matrix[T] {
 	return m
 }
 
-type UpperRightTriangularMatrix[T any] struct {
-	UR *SquareMatrix[T]
-	UL *UpperRightTriangularMatrix[T]
-	BR *UpperRightTriangularMatrix[T]
+func GenericPrintf(value interface{}) string {
+	s := ""
+	switch v := value.(type) {
+	case int:
+		s = fmt.Sprintf("%d", v)
+	case float64:
+		s = fmt.Sprintf("%f", v)
+	case string:
+		s = v
+	case rune:
+		s = fmt.Sprintf("%c", v)
+	default:
+		s = fmt.Sprintf("%v", value)
+	}
+	l := len(s)
+	dv := l / maxLen
+	md := l % maxLen
+	a := strings.Repeat(" ", dv)
+	b := strings.Repeat(" ", md)
+	return strings.Join([]string{a, s, b}, "")
+}
 
-	w, h int
+func (m *UnitMatrix[T]) String() string {
+	if m == nil {
+		return " "
+	}
+	s := GenericPrintf(m.v)
+	// s := fmt.Sprintf("%v", m.v)
+	return s
+}
+
+type EmptyMatrix[T any] struct {
+	w int
+}
+
+func (m *EmptyMatrix[T]) Width() int                     { return m.w }
+func (m *EmptyMatrix[T]) Height() int                    { return m.w }
+func (m *EmptyMatrix[T]) Index(i, j int) T               { return *new(T) }
+func (m *EmptyMatrix[T]) Insert(i, j int, v T) Matrix[T] { return m }
+func (m *EmptyMatrix[T]) Add(a Ring[T]) Ring[T]          { return m }
+func (m *EmptyMatrix[T]) Multiply(a Ring[T]) Ring[T]     { return m }
+
+var maxLen = 1
+
+func (m *EmptyMatrix[T]) String() string {
+	ss := []string{}
+	s := strings.Repeat(" ", (maxLen+1)*m.w)
+	for i := 0; i < m.w; i++ {
+		ss = append(ss, s)
+	}
+	return strings.Join(ss, "\n")
+}
+
+type UpperRightTriangularMatrix[T any] struct {
+	sq *SquareMatrix[T]
+}
+
+func NewUpperRightTriangularMatrix[T any](size int) *UpperRightTriangularMatrix[T] {
+	return &UpperRightTriangularMatrix[T]{sq: NewSquareMatrix[T](size)}
+}
+
+func (t *UpperRightTriangularMatrix[T]) Index(i, j int) T {
+	t.sq.BL = nil
+	return t.sq.Index(i, j)
+}
+
+func (t *UpperRightTriangularMatrix[T]) Insert(i, j int, v T) Matrix[T] {
+	t.sq.BL = nil
+	return t.sq.Insert(i, j, v)
+}
+
+func (t *UpperRightTriangularMatrix[T]) Width() int {
+	t.sq.BL = nil
+	return t.sq.Width()
+}
+
+func (t *UpperRightTriangularMatrix[T]) Height() int {
+	t.sq.BL = nil
+	return t.sq.Height()
+}
+
+func (t *UpperRightTriangularMatrix[T]) Add(b Ring[T]) Ring[T] {
+	t.sq.BL = nil
+	return t.sq.Add(b)
+}
+
+func (t *UpperRightTriangularMatrix[T]) Multiply(b Ring[T]) Ring[T] {
+	t.sq.BL = nil
+	return t.sq.Multiply(b)
+}
+
+// func V[T any](a *UpperRightTriangularMatrix[T], x *SquareMatrix[T], b *UpperRightTriangularMatrix[T]) *SquareMatrix[T] {
+// 	a11, a12, a22 := a.sq.UL.(*UpperRightTriangularMatrix[T]), a.sq.UR.(*SquareMatrix[T]), a.sq.BR.(*UpperRightTriangularMatrix[T])
+// 	x11, x12, x21, x22 := x.UL.(*SquareMatrix[T]), x.UR.(*SquareMatrix[T]), x.BL.(*SquareMatrix[T]), x.BR.(*SquareMatrix[T])
+// 	b11, b12, b22 := b.sq.UL.(*UpperRightTriangularMatrix[T]), b.sq.UR.(*SquareMatrix[T]), b.sq.BR.(*UpperRightTriangularMatrix[T])
+// 	y21 := V(a11, x21, b11)
+// 	y11 := V(a11, x11.Add(a12).Multiply(y21).(*SquareMatrix[T]), b11)
+// 	y22 := V(a22, x22.Add(y21).Multiply(b12).(*SquareMatrix[T]), b22)
+// 	y12 := V(a11, x12.Add(a12).Multiply(y22.Add(y11).Multiply(b12)).(*SquareMatrix[T]), b22)
+// 	return &SquareMatrix[T]{UL: y11, UR: y12, BL: y21, BR: y22}
+// }
+
+func ConstructMatrix(s string) Matrix[rune] {
+	l := len(s)
+	var mat Matrix[rune]
+	size := nextSquare(l)
+	if size == 1 {
+		mat = &UnitMatrix[rune]{}
+	} else {
+		mat = NewSquareMatrix[rune](size)
+	}
+	for i, c := range s {
+		mat.Insert(i, i, c)
+	}
+	return mat
 }
--- sparseMatrix/sparseMatrix_test.go
+++ sparseMatrix/sparseMatrix_test.go
@@ -3,6 +3,7 @@ package sparseMatrix
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -101,14 +102,14 @@ func makeOOBTest(size int) func(t *testing.T) {
 			{size, size, "wowww"},
 			{size, 0, "hehe"}} {
 			sq.Insert(tc.x, tc.y, tc.s)
-			if sq.UL != nil || sq.UR != nil || sq.BL != nil || sq.BR != nil {
+			if !IsEmpty(sq.UL) || !IsEmpty(sq.UR) || !IsEmpty(sq.BL) || !IsEmpty(sq.BR) {
 				t.Fatalf("Invalid inputs %v should have written to no cells", tc)
 			}
 		}
 	}
 }
 
-func TestSquareMatrix(t *testing.T) {
+func TestIterativeSquareMatrix(t *testing.T) {
 	for size := 2; size < 20; size++ {
 		size := size
 		t.Run(fmt.Sprintf("iter-size-%04d", size), func(t *testing.T) {
@@ -139,6 +140,89 @@ func TestSquareMatrix(t *testing.T) {
 	}
 }
 
-func TestAddMatrix(t *testing.T) {
-	// NewSquareMatrix[]()
+// func TestAddMatrix(t *testing.T) {
+// 	// NewSquareMatrix[]()
+// }
+
+func TestConstructMatrix(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  string
+		want Matrix[rune]
+	}{
+		{
+			arg:  "abcdef",
+			name: "test len=6",
+			want: &SquareMatrix[rune]{
+				w: 8,
+				UL: &SquareMatrix[rune]{
+					w: 4,
+					UL: &SquareMatrix[rune]{
+						w:  2,
+						UL: &UnitMatrix[rune]{v: 'a'},
+						UR: &EmptyMatrix[rune]{w: 1},
+						BL: &EmptyMatrix[rune]{w: 1},
+						BR: &UnitMatrix[rune]{v: 'b'},
+					}, // ab
+					UR: &EmptyMatrix[rune]{w: 2},
+					BL: &EmptyMatrix[rune]{w: 2},
+					BR: &SquareMatrix[rune]{
+						w:  2,
+						UL: &UnitMatrix[rune]{v: 'c'},
+						UR: &EmptyMatrix[rune]{w: 1},
+						BL: &EmptyMatrix[rune]{w: 1},
+						BR: &UnitMatrix[rune]{v: 'd'},
+					}, // cd
+				}, // abcd
+				UR: &EmptyMatrix[rune]{w: 4},
+				BL: &EmptyMatrix[rune]{w: 4},
+				BR: &SquareMatrix[rune]{
+					w: 4,
+					UL: &SquareMatrix[rune]{
+						w:  2,
+						UL: &UnitMatrix[rune]{v: 'e'},
+						UR: &EmptyMatrix[rune]{w: 1},
+						BL: &EmptyMatrix[rune]{w: 1},
+						BR: &UnitMatrix[rune]{v: 'f'},
+					}, // ef
+					UR: &EmptyMatrix[rune]{w: 2},
+					BL: &EmptyMatrix[rune]{w: 2},
+					BR: &EmptyMatrix[rune]{w: 2},
+				},
+			},
+		},
+		{
+			arg:  "ab",
+			name: "test len=2",
+			want: &SquareMatrix[rune]{
+				w:  2,
+				UL: &UnitMatrix[rune]{v: 'a'},
+				UR: &EmptyMatrix[rune]{w: 1},
+				BL: &EmptyMatrix[rune]{w: 1},
+				BR: &UnitMatrix[rune]{v: 'b'},
+			},
+		},
+		{
+			arg:  "a",
+			name: "singleton",
+			want: &UnitMatrix[rune]{v: 'a'},
+		},
+		{
+			arg:  "",
+			name: "empty str",
+			want: &UnitMatrix[rune]{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ConstructMatrix(tt.arg); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ConstructMatrix() = %v, want %v", got, tt.want)
+			}
+			s := ConstructMatrix(tt.arg).String()
+			for _, s := range strings.Split(s, "\n") {
+				fmt.Println("|", s, "|")
+			}
+			// fmt.Println(s)
+		})
+	}
 }
