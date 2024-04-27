039fca5747889b69eadef6e714dfe011f799b4cc
a <a@a.a>
2024-04-27 11:19:55 -0700

    Broaden Symbol definition

--- valiant-hs/src/RingParse.hs
+++ valiant-hs/src/RingParse.hs
@@ -12,29 +12,27 @@ import Ring
 import SparseMatrix
 import Vec
 
-type Name = String
-
-data Symbol a = Nonterminal Name | Terminal a
+data Symbol nt t = Nonterminal nt | Terminal t
   deriving (Ord, Eq)
 
-instance Show a => Show (Symbol a) where
-  show (Nonterminal n) = '/' : n ++ "/"
+instance (Show t, Show nt) => Show (Symbol nt t) where
+  show (Nonterminal n) = '/' : show n ++ "/"
   show (Terminal a) = '\'' : show a
 
-data ProductionRule a where
-  Binary :: Name -> Symbol a -> Symbol a -> ProductionRule a
-  Unary :: Name -> Symbol a -> ProductionRule a
+data ProductionRule nt t where
+  Binary :: nt -> Symbol nt t -> Symbol nt t -> ProductionRule t nt
+  Unary :: nt -> Symbol nt t -> ProductionRule t nt
 
-type ProductionRules a = [ProductionRule a]
+type ProductionRules t nt = [ProductionRule t nt]
 
-instance (Show a) => Show (ProductionRule a) where
+instance (Show t, Show nt) => Show (ProductionRule t nt) where
   show rule = show n ++ " -> " ++ concatMap show s
     where
-      s :: [Symbol a]
-      n :: Symbol a
-      (s, n) = case rule of
-        (Unary n a) -> ([a], Nonterminal n)
-        (Binary n a b) -> ([a, b], Nonterminal n)
+      s :: [Symbol nt t]
+      n :: Symbol nt t
+      (n, s) = case rule of
+        (Unary n a) -> (Nonterminal n, [a])
+        (Binary n a b) -> (Nonterminal n, [a, b])
 
 binApp (Binary n a b) x y = if x == a && b == y then return $ Nonterminal n else Nothing
 binApp Unary {} _ _ = Nothing
@@ -54,11 +52,7 @@ instance Applicative f => Applicative (RingParse f) where
   pure = RingParse . pure
   (<*>) (RingParse fs) (RingParse v) = RingParse $ fs <*> v
 
-liftRPF ::
-  (S.Set (Symbol a) -> S.Set (Symbol b) -> S.Set (Symbol c)) ->
-  RingParse S.Set (Symbol a) ->
-  RingParse S.Set (Symbol b) ->
-  RingParse S.Set (Symbol c)
+liftRPF :: (ma a -> mb b -> mc c) -> RingParse ma a -> RingParse mb b -> RingParse mc c
 liftRPF f (RingParse as) (RingParse bs) = RingParse $ f as bs
 
 instance (Applicative f, Semigroup a) => Semigroup (RingParse f a) where
@@ -70,7 +64,7 @@ instance (Monoid a, Ord a, Applicative f, Monoid (f a)) => Monoid (RingParse f a
 instance (f ~ S.Set, Show a) => Show (RingParse f a) where
   show (RingParse s) = show $ S.toList s
 
-productionRules :: ProductionRules String
+productionRules :: ProductionRules String String
 productionRules =
   [ Binary "S" (Nonterminal "NP") (Nonterminal "VP"),
     Binary "VP" (Nonterminal "VP") (Nonterminal "PP"),
@@ -86,7 +80,7 @@ productionRules =
     Unary "Det" (Terminal "a")
   ]
 
-instance (f ~ S.Set, a ~ Symbol String) => Ring (RingParse f a) where
+instance (f ~ S.Set, a ~ Symbol String String) => Ring (RingParse f a) where
   zero = RingParse mempty
   add (RingParse sa) (RingParse sb) = RingParse $ (<>) sa sb
   mul (RingParse x) (RingParse y) = RingParse sc
@@ -100,7 +94,7 @@ applyUnaryOp = catMaybes . (<$> productionRules) . flip unaryApp
 
 unaryOps = applyUnaryOp <$> syms
 
-opRing :: [RingParse S.Set (Symbol String)]
+opRing :: [RingParse S.Set (Symbol String String)]
 opRing = map (RingParse . S.fromList) unaryOps
 
 __f = a `mul` b
@@ -111,7 +105,7 @@ __f = a `mul` b
 -- [[ab  ], [ ], [cd]]
 -- [[    ], [ ]]
 -- [[abcd]]
-applyBinOp :: [[Symbol String]] -> [[Symbol String]]
+applyBinOp :: [[Symbol String String]] -> [[Symbol String String]]
 applyBinOp syms =
   zipWith (\a b -> catMaybes $ binApp <$> productionRules <*> a <*> b) syms $ tail syms
 
