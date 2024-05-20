38c571623bb5f50330e3dddbd66078144e9f61cf
Alexander <bombblob@gmail.com>
2024-05-19 17:42:33 -0700

    add convert

--- valiant-hs/src/Grammar/Convert.hs
+++ valiant-hs/src/Grammar/Convert.hs
@@ -1,7 +1,17 @@
 module Grammar.Convert where
 
+import Data.Set qualified as S
 import Grammar.Chomsky qualified as Chomp
 import Grammar.ContextFree qualified as CF
 
-convert :: CF.CFG -> Chomp.ProductionRules nt t
-convert cfg@(CF.CFG start prods) = undefined
+convert :: CF.CFG -> Chomp.ProductionRules String String
+convert cfg' =
+  let cfg@(CF.CFG _ prods) = if CF.isChomskyReducedForm cfg' then cfg' else CF.toChomskyReducedForm cfg'
+   in if CF.isChomskyReducedForm cfg
+        then map convertRule $ S.toList prods
+        else error "for some reason this grammar in not in Chomsky Reduced Form"
+
+convertRule :: CF.Production -> Chomp.ProductionRule String String
+convertRule (x, xs) = case xs of
+  [a] -> Chomp.Unary x (Chomp.Terminal a)
+  [a, b] -> Chomp.Binary x (Chomp.Nonterminal a []) (Chomp.Nonterminal b [])
